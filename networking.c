ff3d89167ea10ae59606c81be665ab13de5cba37
konstantinnovation <konstans@stuy.edu>
2025-12-17 13:15:28 -0500

    Reimplement TCP handshake functions
    Restored and updated TCP handshake functions for client and server.

--- networking.c
+++ networking.c
@@ -1,37 +1,8 @@
 #include "networking.h"
 
-/*Connect to the server
- *return the to_server socket descriptor
- *blocks until connection is made.*/
-int client_tcp_handshake(char * server_address) {
-
-  //getaddrinfo
-  
-  int serverd;//store the socket descriptor here
-  //create the socket
-  
-  //connect to the server
-  
-  free(hints);
-  freeaddrinfo(results);
-
-  return serverd;
-}
-
-/*Accept a connection from a client
- *return the to_client socket descriptor
- *blocks until connection is made.
- */
-int server_tcp_handshake(int listen_socket){
-    int client_socket;
-
-    //accept the client connection
-  
-    return client_socket;
-}
-
 /*Create and bind a socket.
 * Place the socket in a listening state.
+* returns the socket descriptor
 */
 int server_setup() {
   //setup structs for getaddrinfo
@@ -55,6 +26,40 @@ int server_setup() {
   
   return clientd;
 }
+
+/*Accept a connection from a client
+ *return the socket descriptor for the new sockeet connected to the client
+ *blocks until connection is made.
+ */
+int server_tcp_handshake(int listen_socket){
+    int client_socket;
+
+    //accept() the client connection
+  
+    return client_socket;
+}
+
+
+/*Connect to the server
+ *return the to_server socket descriptor
+ *blocks until connection is made.*/
+int client_tcp_handshake(char * server_address) {
+
+  //getaddrinfo
+  
+  int serverd;//store the socket descriptor here
+  //create the socket
+  
+  //connect() to the server
+  
+  free(hints);
+  freeaddrinfo(results);
+
+  return serverd;
+}
+
+
+
 
 void err(int i, char*message){
   if(i < 0){
